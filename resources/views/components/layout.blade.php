2d8e691cc91dd2dbe2e0a9e37773d31d6d6eb949
t-ookuma <siel0913@gmail.com>
2023-01-24 22:07:32 +0900

    hover action change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,7 +22,7 @@
             <div class="flex items-center space-x-2">
                 <!-- hamburger-menu -->
                 <nav x-data="{ open: false }">
-                    <button class="text-white w-10 h-10 relative focus:outline-none rounded-full hover:bg-gray-500 duration-300" @click="open = !open">
+                    <button class="text-white w-10 h-10 relative focus:outline-none rounded-full hover:bg-gray-500 duration-500" @click="open = !open">
                         <div class="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
                             <span aria-hidden="true" class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
                             <span aria-hidden="true" class="block absolute  h-0.5 w-5 bg-current   transform transition duration-500 ease-in-out" :class="{'opacity-0': open } "></span>
@@ -33,7 +33,7 @@
                         <div class="overflow-hidden rounded-lg ring-1 ring-black ring-opacity-5">
                             <div class="relative text-white grid gap-6  px-5 py-6 sm:gap-8 sm:p-8">
                                 <!-- login -->
-                                <a href="#" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-500">
+                                <a href="#" class="-m-3 flex items-start p-3 hover:border-b hover:border-white">
                                     <svg class="h-6 w-6 flex-shrink-0 text-template-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
                                     </svg>
@@ -42,7 +42,7 @@
                                     </div>
                                 </a>
                                 <!-- logout -->
-                                <a href="#" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-500">
+                                <a href="#" class="-m-3 flex items-start p-3 hover:border-b hover:border-white">
                                     <svg class="h-6 w-6 flex-shrink-0 text-template-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9" />
                                     </svg>
@@ -59,8 +59,8 @@
             </div>
             <div class="flex items-center font-bold text-white">
                 <!-- dropdown-menu -->
-                <div class="relative rounded-lg hover:bg-gray-500" x-data="{ open: false }">
-                    <button @click="open = ! open" type="button" class="text-white group p-4 inline-flex items-center text-base font-medium hover:text-white" aria-expanded="false">
+                <div class="relative" x-data="{ open: false }">
+                    <button @click="open = ! open" type="button" class="text-white group px-4 pt-4 pb-2 inline-flex items-center text-base font-medium hover:text-white hover:border-b hover:border-white" aria-expanded="false">
                         @auth
                         <span class="cursor-pointer text-lg">{{ Auth::user->name }}</span>
                         @endauth
@@ -75,7 +75,7 @@
                         <div class="overflow-hidden rounded-lg ring-1 ring-black ring-opacity-5">
                             <div class="relative grid gap-6  px-5 py-6 sm:gap-8 sm:p-8">
                                 <!-- login -->
-                                <a href="#" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-500">
+                                <a href="#" class="-m-3 flex items-start p-3 hover:border-b hover:border-white">
                                     <svg class="h-6 w-6 flex-shrink-0 text-template-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
                                     </svg>
@@ -84,7 +84,7 @@
                                     </div>
                                 </a>
                                 <!-- logout -->
-                                <a href="#" class="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-500">
+                                <a href="#" class="-m-3 flex items-start p-3 hover:border-b hover:border-white">
                                     <svg class="h-6 w-6 flex-shrink-0 text-template-secondary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9" />
                                     </svg>
